2831272514ec1a3af12e4de9f664bbc52e0c8f6f
Manuel Werder <manuel.werder@csbe.ch>
2020-02-25 12:30:31 +0100

    Created BombermanServerProxy class, this will be the path to the server in the network

--- src/main/java/BombermanClient/Bomberman/Bomberman.java
+++ src/main/java/BombermanClient/Bomberman/Bomberman.java
@@ -7,15 +7,8 @@ import BombermanClient.UserInterface.UserKeyboardInput;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Scanner;
 
 public class Bomberman extends JFrame implements GameConstants {
-	private static final String HOST = "127.0.0.1";
-	private static final int PORT = 8765;
 
 	UserKeyboardInput keyboardInput = new UserKeyboardInput();
 
@@ -33,33 +26,6 @@ public class Bomberman extends JFrame implements GameConstants {
 
 		pack();
 		setVisible(true);
-	}
-
-	public void runClient() {
-		try (Socket socket = new Socket(HOST, PORT);
-		     BufferedReader in = new BufferedReader(
-		     		new InputStreamReader(socket.getInputStream()));
-		     PrintWriter out = new PrintWriter(
-		     		socket.getOutputStream(), true);
-		     Scanner sc = new Scanner(System.in))
-		{
-
-			System.out.println(in.readLine());
-
-			System.out.print("> ");
-			String line = sc.nextLine();
-			if (line.length() == 0)
-				return;
-			out.println(line);
-			System.out.println("Antwort vom Server:");
-			System.out.println(in.readLine());
-
-			repaint();
-
-		} catch (Exception e) {
-			//noinspection ThrowablePrintedToSystemOut
-			System.err.println(e);
-		}
 	}
 
 	private void loadServerLogin() {
